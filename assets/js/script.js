eba19099e95f1f23782bb9ae0aa1406c7176b42a
Enrico Perkins Jr <evperkinsjr@gmail.com>
2021-05-21 16:30:09 -0400

    pseudo-coding the javascript

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// variables
+
+// quiz questions & answers
+
+// generate questions & answers
+
+// start timer, display first question
+
+// check answers
+
+// display results
+
+// user submits score, add score to high scores list
+
+// generate new high score list
+
+// display high scores
+
+// clear high scores list
+
+// replay quiz
+
+// start quiz
